debc3342af43221416f159c262e8c66e8ac2a590
ubaid4j <ubaidshan007@gmail.com>
2025-02-18 11:59:53 +0500

    - send message to kafka

--- src/main/java/dev/ubaid/kp/App.java
+++ src/main/java/dev/ubaid/kp/App.java
@@ -2,7 +2,11 @@ package dev.ubaid.kp;
 
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.time.Duration;
 import java.util.Properties;
 
 /**
@@ -10,7 +14,10 @@ import java.util.Properties;
  *
  */
 public class App {
-    public static void main( String[] args ) {
+    
+    static Logger log = LoggerFactory.getLogger(App.class);
+    
+    public static void main( String[] args ) throws InterruptedException {
 
         Properties kafkaProps = new Properties();
         kafkaProps.put("bootstrap.servers", "localhost:9092");
@@ -22,5 +29,30 @@ public class App {
         KafkaProducer<String, String> kafkaProducer = new KafkaProducer<>(kafkaProps);
 
         ProducerRecord<String, String> record = new ProducerRecord<>("cutsomer-country", "Precision Products", "France");
+        
+        try {
+            kafkaProducer.send(record);
+        } catch (Exception e) {
+            log.error("error: ", e);
+            throw new RuntimeException(e);
+        }
+
+
+        ProducerRecord<String, String> record2 = new ProducerRecord<>("cutsomer-country","Precision Products", "Germany");
+
+        try {
+            RecordMetadata metadata = kafkaProducer.send(record2, (metadata_, exp_) -> {
+                log.error("{} is not sent. Error: ", metadata_, exp_);
+            }).get();
+            log.info("message sent, meta data: {}", metadata.toString());
+        } catch (Exception e) {
+            log.error("error: ", e);
+            throw new RuntimeException(e);
+        }
+
+
+        Thread.sleep(Duration.ofSeconds(20));
     }
+    
+    
 }
